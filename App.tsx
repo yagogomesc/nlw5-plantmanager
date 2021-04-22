5bd8eca62edc38df287f0770aec6379a47cc794e
Yago Gomes <yagogomesc@gmail.com>
2021-04-22 10:31:35 -0300

    Created first screen and change assets

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Welcome } from "./src/pages/welcome";
 
 export default function App() {
-  return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>NLW#5</Text>
-    </View>
-  );
+  return <Welcome />;
 }
