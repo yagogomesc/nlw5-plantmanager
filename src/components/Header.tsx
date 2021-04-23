8510c04d9350db89e94c5be67dfd02a5aad6da41
Yago Gomes <yagogomesc@gmail.com>
2021-04-23 14:12:16 -0300

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+import colors from "../styles/colors";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import userImg from "../assets/yago.png";
+import fonts from "../styles/fonts";
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>Olá,</Text>
+        <Text style={styles.userName}>Yago</Text>
+      </View>
+      <Image source={userImg} style={styles.image} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight(),
+    padding: 20,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 40,
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+});
