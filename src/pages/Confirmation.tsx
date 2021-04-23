e36e123203ef76236010f77fe2f67c906f304b9d
Yago Gomes <yagogomesc@gmail.com>
2021-04-23 14:12:35 -0300

    Create PlantSelect and include route

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { StyleSheet, SafeAreaView, Text, View } from "react-native";
 
@@ -6,6 +7,12 @@ import colors from "../styles/colors";
 import fonts from "../styles/fonts";
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate("PlantSelect");
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -17,7 +24,7 @@ export function Confirmation() {
           Agora vamos começar a cuidar das suas plantinhas com muito cuidado.
         </Text>
         <View style={styles.footer}>
-          <Button title="Começar" />
+          <Button title="Começar" onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import colors from "../styles/colors";
+import { Header } from "../components/Header";
+
+export function PlantSelect() {
+  return (
+    <View style={styles.container}>
+      <Header />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
