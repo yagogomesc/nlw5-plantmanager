02c88b97dde67a843c264fa0f34bbe9573852fa2
Yago Gomes <yagogomesc@gmail.com>
2021-04-24 07:58:12 -0300

    Add header to Plant select page

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import colors from "../styles/colors";
 import { Header } from "../components/Header";
+import fonts from "../styles/fonts";
 
 export function PlantSelect() {
   return (
     <View style={styles.container}>
-      <Header />
+      <View style={styles.header}>
+        <Header />
+
+        <Text style={styles.title}>Em qual ambiente</Text>
+        <Text style={styles.subtitle}>você quer colocar sua planta?</Text>
+      </View>
     </View>
   );
 }
@@ -15,5 +21,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.background,
+  },
+  header: {
+    paddingHorizontal: 30,
+  },
+  title: {
+    fontSize: 17,
+    color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 20,
+    marginTop: 15,
+  },
+  subtitle: {
+    fontFamily: fonts.text,
+    fontSize: 17,
+    lineHeight: 20,
+    color: colors.heading,
   },
 });
