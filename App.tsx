88adeea0fa4db514c9da2b564ad1576c8782610e
Yago Gomes <yagogomesc@gmail.com>
2021-04-30 11:42:19 -0300

    Include on APP subscription to listen notifications

--- App.tsx
+++ App.tsx
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Routes from "./src/routes";
 import {
   useFonts,
   Jost_400Regular,
   Jost_600SemiBold,
 } from "@expo-google-fonts/jost";
-import { Text, View } from "react-native";
+
+import * as Notifications from "expo-notifications";
+
 import AppLoading from "expo-app-loading";
+import { PlantProps } from "./src/libs/storage";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold,
   });
+
+  useEffect(() => {
+    // const subscription = Notifications.addNotificationReceivedListener(
+    //   async (notification) => {
+    //     const data = notification.request.content.data.plant as PlantProps;
+    //     console.log(data);
+    //   }
+    // );
+
+    // return () => subscription.remove();
+
+    async function notifications() {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+      const data = await Notifications.getAllScheduledNotificationsAsync();
+      console.log(data);
+    }
+
+    notifications();
+  }, []);
 
   if (!fontsLoaded) return <AppLoading />;
 
